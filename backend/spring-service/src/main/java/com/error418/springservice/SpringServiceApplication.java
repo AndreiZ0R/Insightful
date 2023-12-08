f0e3ed7717fe3d8d9e1f1f3784d24926059445d3
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-12-08 17:58:45 +0200

    projects setup

---
+++ backend/spring-service/src/main/java/com/error418/springservice/SpringServiceApplication.java
@@ -0,0 +1,12 @@
+package com.error418.springservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceApplication.class, args);
+    }
+}
---
+++ backend/spring-service/src/test/java/com/error418/springservice/SpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.error418.springservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
